4d2dba53654dabca31e11b651fb8a57d7f4ba501
anmolhuro <anmol.nagpal@hurodata.com>
2023-03-11 00:53:33 +0530

    initial commit

--- _example/complete/main.tf
+++ _example/complete/main.tf
@@ -1,1 +1,27 @@
-locals {}
+provider "azurerm" {
+  features {}
+}
+
+## Resource Group
+module "resource_group" {
+  source = "clouddrove/resource-group/azure"
+
+  label_order = ["name", "environment", ]
+  name        = "app"
+  environment = "test"
+  location    = "North Europe"
+}
+
+module "data_factory" {
+  depends_on = [
+    module.resource_group
+  ]
+  source = "../../"
+
+  name                = "app"
+  environment         = "test"
+  label_order         = ["name", "environment"]
+  enabled             = true
+  location            = module.resource_group.resource_group_location
+  resource_group_name = module.resource_group.resource_group_name
+}
